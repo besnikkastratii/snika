9bea2095c3b2cfc2b10ac3f9bc2abfd4080d3187
besnikkastratii <besnikkastrati2@gmail.com>
2022-12-03 23:29:51 +0100

    commit 5

--- server.php
+++ server.php
@@ -3,8 +3,8 @@ include('db.php');
 session_start();
 // initializing variables
 $username = "";
-$email    = "";
-$errors = array(); 
+$email = "";
+$errors = array();
 
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'besnik1');
@@ -19,12 +19,20 @@ if (isset($_POST['reg_user'])) {
   $pubgidd = mysqli_real_escape_string($db, $_POST['pubgidd']);
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-    if (empty($pubgidd)) { array_push($errors, "ID is required"); }
+  if (empty($username)) {
+    array_push($errors, "Username is required");
+  }
+  if (empty($email)) {
+    array_push($errors, "Email is required");
+  }
+  if (empty($password_1)) {
+    array_push($errors, "Password is required");
+  }
+  if (empty($pubgidd)) {
+    array_push($errors, "ID is required");
+  }
   if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+    array_push($errors, "The two passwords do not match");
   }
 
   // first check the database to make sure 
@@ -32,45 +40,45 @@ if (isset($_POST['reg_user'])) {
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' OR pubgid='$pubgidd' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
-  
+
   if ($user) { // if user exists
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
- }
-if ($user['pubgid'] === $pubgidd) {
+    }
+    if ($user['pubgid'] === $pubgidd) {
       array_push($errors, "id already exists");
     }
     if ($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
 
-  
 
-	if (strlen($pubgidd) < 6){
-   array_push($errors, "pubg id is too short");
-}
-	
-		if (strlen($username) < 5){
-   array_push($errors, "username  is too short,minimum 5 characters");
-}
 
-		if (strlen($password_1) < 6){
-   array_push($errors, "password is too short,minimum 6 characters");
-}
+    if (strlen($pubgidd) < 6) {
+      array_push($errors, "pubg id is too short");
+    }
+
+    if (strlen($username) < 5) {
+      array_push($errors, "username  is too short,minimum 5 characters");
+    }
+
+    if (strlen($password_1) < 6) {
+      array_push($errors, "password is too short,minimum 6 characters");
+    }
   }
 
-$end_date  = date("Y/m/d H:i:s", strtotime($date . ' + 10 days'));
+  $end_date = date("Y/m/d H:i:s", strtotime($date . ' + 10 days'));
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+    $password = md5($password_1); //encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (username, email, password, pubgid, registrationD, expiryD) 
+    $query = "INSERT INTO users (username, email, password, pubgid, registrationD, expiryD) 
   			  VALUES('$username', '$email', '$password', '$pubgidd', '$date', '$end_date')";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are Registred";
-  	header('location: index.php');
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are Registred";
+    header('location: index.php');
   }
 }
 
@@ -81,44 +89,42 @@ if (isset($_POST['login_user'])) {
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username)) {
-  	array_push($errors, "username can not be empty");
+    array_push($errors, "username can not be empty");
   }
   if (empty($password)) {
-  	array_push($errors, "password can not be empty");
+    array_push($errors, "password can not be empty");
   }
-  if (strlen($username) < 5){
-   array_push($errors, "username  is too short,minimum 6 characters required");
-}
+  if (strlen($username) < 5) {
+    array_push($errors, "username  is too short,minimum 6 characters required");
+  }
 
-$query = "SELECT * FROM users WHERE username='$username' or email='$username' or pubgid='$username' AND password='$password'";
-$result=mysqli_query($db,$query);
-$num=mysqli_num_rows($result);
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
-if($row['expiryD'] < $date)
-	  {
- array_push($errors, "Your account has Expired");
-}	
-
+  $query = "SELECT * FROM users WHERE username='$username' or email='$username' or pubgid='$username' AND password='$password'";
+  $result = mysqli_query($db, $query);
+  $num = mysqli_num_rows($result);
+  while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+    if ($row['expiryD'] < $date) {
+      array_push($errors, "Your account has Expired");
+    }
 
 
 
 
 
-	
+
+
   if (count($errors) == 0) {
-  	$password = md5($password);
-  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  	$results = mysqli_query($db, $query);
-  	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-	 
-  	  header('location: index.php');
-  	}
-	else {
-  		array_push($errors, "Wrong username/password combination");
-  	}
+    $password = md5($password);
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      $_SESSION['success'] = "You are now logged in";
 
-}
+      header('location: index.php');
+    } else {
+      array_push($errors, "Wrong username/password combination");
+    }
+
+  }
 }
 ?>
